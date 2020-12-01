7f96f7afb3974547916915daf9415d86fd008ef4
saurabh <sharmasaurabh2412@gmail.com>
2020-12-01 09:27:26 +0530

    fix : fixed mobile view background image

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const inputText = document.querySelector('#input-text');
 const outputText = document.querySelector('#output-text');
+const spinner = document.querySelector('#spinner');
+
+/* function showSpinner() {
+    spinner.className = "show";
+    setTimeout(() => {
+      spinner.className = spinner.className.replace("show", "");
+    }, 5000);
+} */
 
 const btnTranslator = document.querySelector('#btn-translate');
 btnTranslator.addEventListener('click', () => {
+    /* showSpinner() */
     const URL = "https://api.funtranslations.com/translate/minion.json?text=" + inputText.value
     const response = fetch(URL)
     .then(response => response.json())
